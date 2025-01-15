8b6a13f564ae661fd155346f59e17fa63c75d5a3
Akhpie <akhya1819@gmail.com>
2025-01-15 19:34:30 +0530

    removed methods

--- server/src/index.ts
+++ server/src/index.ts
@@ -45,7 +45,6 @@ app.use(
   cors({
     origin: allowedOrigins,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
     exposedHeaders: ["set-cookie"],
   })
