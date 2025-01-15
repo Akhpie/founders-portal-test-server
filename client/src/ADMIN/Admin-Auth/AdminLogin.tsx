7b19f820c8a75acf3780c0442ba68389a0498927
Akhpie <akhya1819@gmail.com>
2025-01-15 21:00:37 +0530

    testing

--- client/src/ADMIN/Admin-Auth/AdminLogin.tsx
+++ client/src/ADMIN/Admin-Auth/AdminLogin.tsx
@@ -136,7 +136,7 @@ const AdminLogin: React.FC = () => {
             credential: credentialResponse.credential,
           }),
           credentials: "include",
-          mode: "cors",
+          mode: "no-cors",
         }
       );
 
