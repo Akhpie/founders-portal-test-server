bb67cc9aac2cde92d771f543d362a7fa3d269ea6
Akhpie <akhya1819@gmail.com>
2025-01-15 19:08:31 +0530

    testing

--- server/src/index.ts
+++ server/src/index.ts
@@ -44,12 +44,11 @@ app.use(cookieParser());
 // );
 app.use(
   cors({
-    origin: "https://founders-portal-test-server-client.onrender.com", // Exact frontend URL
+    origin: allowedOrigins,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
     exposedHeaders: ["set-cookie"],
-    preflightContinue: true, // Add this
   })
 );
 
