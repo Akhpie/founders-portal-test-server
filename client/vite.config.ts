e4e166eddedf513a41631704c15194245dc4b55d
Akhpie <akhya1819@gmail.com>
2025-01-19 20:42:45 +0530

    increased chunk size

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase chunk size warning limit to 1 MB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Split node_modules into separate chunks by vendor
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
