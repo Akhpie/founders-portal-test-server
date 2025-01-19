1e2e250fd53977ce32439f89d37abd6aec7be19c
Akhpie <akhya1819@gmail.com>
2025-01-19 19:09:06 +0530

    made a few more changes

--- server/src/index.js
+++ server/src/index.js
@@ -34,6 +34,7 @@ const influencerRoutes = require("./routes/influencer-routes/influencerRoutes");
 const StartupInfluencerRoutes = require("./routes/influencer-routes/StartupInfluencerRoutes");
 
 dotenv_1.default.config();
+
 // Middleware
 app.use(
   (0, cors_1.default)({
@@ -51,10 +52,6 @@ app.use((req, res, next) => {
 app.use(express_1.default.json());
 // Connect to Database
 (0, db_1.default)();
-// Basic Route
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
 
 // Routes
 app.use("/api/auth", googleAuthRoutes);
@@ -75,6 +72,12 @@ app.use("/api", templateRoutes);
 app.use("/api/resources", resourceRoutes);
 app.use("/api/influencers", influencerRoutes);
 app.use("/api/startup-influencers", StartupInfluencerRoutes);
+
+// Basic Route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 exports.default = app;
--- server/src/routes/google-auth/googleAuthRoutes.ts
+++ server/src/routes/google-auth/googleAuthRoutes.ts
@@ -1,33 +1,3 @@
-// import express, { RequestHandler } from "express";
-// import { googleLogin } from "../../controllers/googleauthController";
-// import { authenticateAdmin } from "../../middleware/adminAuth";
-
-// const router = express.Router();
-
-// // Get current user info
-// const getCurrentUser: RequestHandler = (req, res) => {
-//   res.json({
-//     id: req.adminuser?._id,
-//     adminemail: req.adminuser?.adminemail,
-//     adminname: req.adminuser?.adminname,
-//     adminrole: req.adminuser?.adminrole,
-//     profilePic: req.adminuser?.profilePic,
-//   });
-// };
-
-// // Logout handler
-// const logout: RequestHandler = (req, res) => {
-//   res.clearCookie("adminToken");
-//   res.json({ message: "Logged out successfully" });
-// };
-
-// // Route definitions
-// router.post("/google-login", googleLogin);
-// router.get("/current-user", authenticateAdmin, getCurrentUser);
-// router.post("/logout", logout);
-
-// export default router;
-
 import express, { RequestHandler } from "express";
 import { googleLogin } from "../../controllers/googleauthController";
 import { authenticateAdmin } from "../../middleware/adminAuth";
