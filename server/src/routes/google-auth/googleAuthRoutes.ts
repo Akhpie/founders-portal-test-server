33a4c6c75271d4ce613813a4e0b0fe73c3f6df41
Akhpie <akhya1819@gmail.com>
2025-01-20 18:23:51 +0530

    a minor change

--- server/src/routes/google-auth/googleAuthRoutes.ts
+++ server/src/routes/google-auth/googleAuthRoutes.ts
@@ -1,14 +1,65 @@
+// import express, { RequestHandler } from "express";
+// import { googleLogin } from "../../controllers/googleauthController";
+// import { authenticateAdmin } from "../../middleware/adminAuth";
+
+// const router = express.Router();
+// const FRONTEND_URL = "https://founders-portal-test-server-client.onrender.com";
+
+// // Get current user info
+// const getCurrentUser: RequestHandler = (req, res) => {
+//   res.header("Access-Control-Allow-Origin", FRONTEND_URL);
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.json({
+//     id: req.adminuser?._id,
+//     adminemail: req.adminuser?.adminemail,
+//     adminname: req.adminuser?.adminname,
+//     adminrole: req.adminuser?.adminrole,
+//     profilePic: req.adminuser?.profilePic,
+//   });
+// };
+
+// // Logout handler
+// const logout: RequestHandler = (req, res) => {
+//   res.header("Access-Control-Allow-Origin", FRONTEND_URL);
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.clearCookie("adminToken", {
+//     httpOnly: true,
+//     secure: true,
+//     sameSite: "none",
+//   });
+//   res.json({ message: "Logged out successfully" });
+// };
+
+// // Wrap googleLogin to ensure proper headers
+// const wrappedGoogleLogin: RequestHandler = async (req, res, next) => {
+//   console.log("Google login request received");
+//   try {
+//     const origin = req.headers.origin;
+//     if (origin === FRONTEND_URL) {
+//       res.header("Access-Control-Allow-Origin", FRONTEND_URL);
+//       res.header("Access-Control-Allow-Credentials", "true");
+//     }
+//     await googleLogin(req, res, next);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+// // Route definitions
+// router.post("/google-login", wrappedGoogleLogin);
+// router.get("/current-user", authenticateAdmin, getCurrentUser);
+// router.post("/logout", logout);
+
+// export default router;
+
 import express, { RequestHandler } from "express";
 import { googleLogin } from "../../controllers/googleauthController";
 import { authenticateAdmin } from "../../middleware/adminAuth";
 
 const router = express.Router();
-const FRONTEND_URL = "https://founders-portal-test-server-client.onrender.com";
 
 // Get current user info
 const getCurrentUser: RequestHandler = (req, res) => {
-  res.header("Access-Control-Allow-Origin", FRONTEND_URL);
-  res.header("Access-Control-Allow-Credentials", "true");
   res.json({
     id: req.adminuser?._id,
     adminemail: req.adminuser?.adminemail,
@@ -20,33 +71,12 @@ const getCurrentUser: RequestHandler = (req, res) => {
 
 // Logout handler
 const logout: RequestHandler = (req, res) => {
-  res.header("Access-Control-Allow-Origin", FRONTEND_URL);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.clearCookie("adminToken", {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
-  });
+  res.clearCookie("adminToken");
   res.json({ message: "Logged out successfully" });
 };
 
-// Wrap googleLogin to ensure proper headers
-const wrappedGoogleLogin: RequestHandler = async (req, res, next) => {
-  console.log("Google login request received");
-  try {
-    const origin = req.headers.origin;
-    if (origin === FRONTEND_URL) {
-      res.header("Access-Control-Allow-Origin", FRONTEND_URL);
-      res.header("Access-Control-Allow-Credentials", "true");
-    }
-    await googleLogin(req, res, next);
-  } catch (error) {
-    next(error);
-  }
-};
-
 // Route definitions
-router.post("/google-login", wrappedGoogleLogin);
+router.post("/google-login", googleLogin);
 router.get("/current-user", authenticateAdmin, getCurrentUser);
 router.post("/logout", logout);
 
